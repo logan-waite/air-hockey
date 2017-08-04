d86494679c47cab27e7295aa65e974f73abd0a61
Logan Waite <lwaite2010@gmail.com>
2017-08-04 10:12:25 -0600

    Added file to create the database

---
+++ dbcreate.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect("/var/www/airhockey/airhockey.sqlite")
+conn.text_factory = str
+cur = conn.cursor()
+
+cur.execute(""" CREATE TABLE "Matches" (
+                `match_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                `p1_id` INTEGER NOT NULL,
+                `p2_id` INTEGER NOT NULL,
+                `p1_score` INTEGER NOT NULL,
+                `p2_score` INTEGER NOT NULL,
+                `p1_weight` INTEGER,
+                `p2_weight` INTEGER,
+                FOREIGN KEY(`p1_id`) REFERENCES `Player`(`player_id`),
+                FOREIGN KEY(`p2_id`) REFERENCES `Player`(`player_id`)
+                ) """)
+cur.execute(""" CREATE TABLE "Players" (
+                `player_Id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                `name` TEXT NOT NULL UNIQUE,
+                `wins` INTEGER DEFAULT 0,
+                `weight` INTEGER DEFAULT 0
+                ) """)
+conn.commit()
+conn.close()
