cb5e9a0e96305982bd1b038c1e6766cb471efe3a
Logan Waite <lwaite2010@gmail.com>
2017-08-04 10:13:53 -0600

    Added try/excepts

--- dbcreate.py
+++ dbcreate.py
@@ -1,25 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect("/var/www/airhockey/airhockey.sqlite")
-conn.text_factory = str
-cur = conn.cursor()
+try:
+    conn = sqlite3.connect("/var/www/airhockey/airhockey.sqlite")
+    conn.text_factory = str
+    cur = conn.cursor()
+except:
+    print("Was unable to create the database")
 
-cur.execute(""" CREATE TABLE "Matches" (
-                `match_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-                `p1_id` INTEGER NOT NULL,
-                `p2_id` INTEGER NOT NULL,
-                `p1_score` INTEGER NOT NULL,
-                `p2_score` INTEGER NOT NULL,
-                `p1_weight` INTEGER,
-                `p2_weight` INTEGER,
-                FOREIGN KEY(`p1_id`) REFERENCES `Player`(`player_id`),
-                FOREIGN KEY(`p2_id`) REFERENCES `Player`(`player_id`)
-                ) """)
-cur.execute(""" CREATE TABLE "Players" (
-                `player_Id` INTEGER PRIMARY KEY AUTOINCREMENT,
-                `name` TEXT NOT NULL UNIQUE,
-                `wins` INTEGER DEFAULT 0,
-                `weight` INTEGER DEFAULT 0
-                ) """)
+try:
+    cur.execute(""" CREATE TABLE "Matches" (
+                    `match_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                    `p1_id` INTEGER NOT NULL,
+                    `p2_id` INTEGER NOT NULL,
+                    `p1_score` INTEGER NOT NULL,
+                    `p2_score` INTEGER NOT NULL,
+                    `p1_weight` INTEGER,
+                    `p2_weight` INTEGER,
+                    FOREIGN KEY(`p1_id`) REFERENCES `Player`(`player_id`),
+                    FOREIGN KEY(`p2_id`) REFERENCES `Player`(`player_id`)
+                    ) """)
+    cur.execute(""" CREATE TABLE "Players" (
+                    `player_Id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                    `name` TEXT NOT NULL UNIQUE,
+                    `wins` INTEGER DEFAULT 0,
+                    `weight` INTEGER DEFAULT 0
+                    ) """)
+except:
+    print("Was unable to create the tables")
+
+print("Success!")
 conn.commit()
 conn.close()
